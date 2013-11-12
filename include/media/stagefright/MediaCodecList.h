Jackeagle/caf_frameworks_av

libstagefright: Add support for H/W AAC decoder
- Currently, only software AAC decoding is supported.
- Add support for H/W AAC decoding by including it in the
  list of available decoders and use it for decoding only
  if the property 'media.aaccodectype' is set to 0.

Change-Id: I4bb9df1bd10bd8ee91e63dadd6c473fc4e29813a
CRs-Fixed: 449145

include/media/stagefright/MediaCodecList.h
@@ -99,6 +99,8 @@ private:
     status_t addTypeFromAttributes(const char **attrs);
     void addType(const char *name);
 
+    friend class QCUtilityClass;
+
     DISALLOW_EVIL_CONSTRUCTORS(MediaCodecList);
 };
 

media/libstagefright/MediaCodecList.cpp
@@ -27,6 +27,7 @@
 #include <utils/threads.h>
 
 #include <libexpat/expat.h>
+#include "include/QCUtilityClass.h"
 
 namespace android {
 
@@ -64,6 +65,12 @@ MediaCodecList::MediaCodecList()
 
         addMediaCodec(
                 false /* encoder */, "OMX.google.raw.decoder", "audio/raw");
+
+        Vector<AString> QcomAACQuirks;
+        QcomAACQuirks.push(AString("requires-allocate-on-input-ports"));
+        QcomAACQuirks.push(AString("requires-allocate-on-output-ports"));
+        QCUtilityClass::helper_addMediaCodec(mCodecInfos, mTypes, false, "OMX.qcom.audio.decoder.multiaac",
+            "audio/mp4a-latm", QCUtilityClass::helper_getCodecSpecificQuirks(mCodecQuirks, QcomAACQuirks));
     }
 
 #if 0

inimeg81/frameworks_av

include/media/stagefright/MediaCodecList.h
@@ -99,6 +99,8 @@ private:
     status_t addTypeFromAttributes(const char **attrs);
     void addType(const char *name);
 
+    friend class QCUtils;
+
     DISALLOW_EVIL_CONSTRUCTORS(MediaCodecList);
 };
 

media/libstagefright/MediaCodecList.cpp
@@ -27,6 +27,7 @@
 #include <utils/threads.h>
 
 #include <libexpat/expat.h>
+#include "include/QCUtils.h"
 
 namespace android {
 
@@ -67,6 +68,12 @@ MediaCodecList::MediaCodecList()
 
         addMediaCodec(
                 false /* encoder */, "OMX.google.raw.decoder", "audio/raw");
+
+        Vector<AString> QcomAACQuirks;
+        QcomAACQuirks.push(AString("requires-allocate-on-input-ports"));
+        QcomAACQuirks.push(AString("requires-allocate-on-output-ports"));
+        QCUtils::helper_addMediaCodec(mCodecInfos, mTypes, false, "OMX.qcom.audio.decoder.multiaac",
+            "audio/mp4a-latm", QCUtils::helper_getCodecSpecificQuirks(mCodecQuirks, QcomAACQuirks));
     }
 
 #if 0